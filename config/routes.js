e900712511d0fa988515201695197b7dd05421c6
ngallagher87 <nathan@discoverysoftware.com>
2013-08-01 09:30:33 -0700

    Configuring some indentation

--- config/routes.js
+++ config/routes.js
@@ -6,9 +6,9 @@ var async = require('async');
 
 module.exports = function (app) {
 	// Load our soldier route
-	var soldier_ctrl 	= require('../app/controllers/soldier_controller'),
-		redGod			= require('../app/controllers/red_god'),
-		passport		= require('passport');	 
+  var soldier_ctrl  = require('../app/controllers/soldier_controller'),
+      redGod        = require('../app/controllers/red_god'),
+      passport      = require('passport');
 
 	// Route to do our battling
 	app.get('/battle', ensureAuthenticated, function(req, res) {
@@ -20,7 +20,7 @@ module.exports = function (app) {
 			function(callback) {
 				var soldier2 = soldier_ctrl.loadSoldier(2, callback);
 			}
-		], 
+		],
 		function (err, result) {
 		   // result now has both our soldiers
 		   // If it doesnt, output an error
@@ -31,14 +31,14 @@ module.exports = function (app) {
 		   }
 		});
 	});
-	
+
 	// Create some soldiers
 	app.get('/save', ensureAuthenticated, function(req, res) {
 		soldier_ctrl.createSoldier(req, res, 1, "Hank");
 		soldier_ctrl.createSoldier(req, res, 2, "Urgramesh the Destroyer");
 		res.send("Created two soldiers.\n", 200);
 	});
-		
+
 	// Route to show winner history
 	app.get('/winners', ensureAuthenticated, function(req, res) {
 		async.parallel([
@@ -54,18 +54,18 @@ module.exports = function (app) {
 			res.send(JSON.stringify(result[0], null, 4) + "\n" + JSON.stringify(result[1], null, 4), 200);
 		});
 	});
-	
+
 	// Note:
 	// Passport code taken from https: //github.com/jaredhanson/passport-github/blob/master/examples/login/app.js
 	// All credits go to Jared Hanson for this auth code
 	app.get('/account', ensureAuthenticated, function(req, res){
 		res.render('account', { user: req.user });
 	});
-	
+
 	app.get('/login', function(req, res){
 		res.render('login', { user: req.user });
 	});
-	
+
 	// GET /auth/github
 	//   Use passport.authenticate() as route middleware to authenticate the
 	//   request.  The first step in GitHub authentication will involve redirecting
@@ -77,28 +77,28 @@ module.exports = function (app) {
 	    // The request will be redirected to GitHub for authentication, so this
 	    // function will not be called.
 	});
-	
+
 	// GET /auth/github/callback
 	//   Use passport.authenticate() as route middleware to authenticate the
 	//   request.  If authentication fails, the user will be redirected back to the
 	//   login page.  Otherwise, the primary route function function will be called,
 	//   which, in this example, will redirect the user to the battle page.
-	app.get('/auth/github/callback', 
+	app.get('/auth/github/callback',
 		passport.authenticate('github', { failureRedirect: '/login' }),
 		function(req, res) {
 			res.redirect('/battle');
 	});
-	
+
 	app.get('/logout', function(req, res){
 		req.logout();
 		res.redirect('/login');
 	});
-	
+
 	// 404 handler
 	app.get('*', function(req, res) {
 		res.send('404 - what???', 404);
 	});
-	
+
 	// Simple route middleware to ensure user is authenticated.
 	//   Use this route middleware on any resource that needs to be protected.  If
 	//   the request is authenticated (typically via a persistent login session),
